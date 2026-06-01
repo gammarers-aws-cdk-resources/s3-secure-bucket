b5e4df4d9bfefdf909f2199777fb237f4800f58f
yicr <yicr@users.noreply.github.com>
2026-06-01 05:14:59 +0000

    chore: add package description

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,6 +10,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   packageManager: javascript.NodePackageManager.YARN_CLASSIC,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers-aws-cdk-resources/s3-secure-bucket.git',
+  description: 'S3 Secure Bucket is a construct that creates a secure bucket with encryption, logging, and other security features.',
   releaseToNpm: true,
   npmTrustedPublishing: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
